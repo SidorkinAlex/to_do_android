ed4436bf639a1cbf42a60d4a5d3734fe5456f2e3
SidorkinAlex <palach.orl@mail.ru>
2021-10-06 23:10:56 +0300

    updating mainActivity

--- app/src/main/java/suitecrm/com/todochecklist/MainActivity.kt
+++ app/src/main/java/suitecrm/com/todochecklist/MainActivity.kt
@@ -2,8 +2,10 @@ package suitecrm.com.todochecklist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import suitecrm.com.todochecklist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBuinding : ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
