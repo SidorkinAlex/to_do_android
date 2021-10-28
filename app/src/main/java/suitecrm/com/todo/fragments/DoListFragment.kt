7a74c5fe9f36cf866e62e3c05099f7697e3b7ece
SidorkinAlex <palach.orl@mail.ru>
2021-10-29 00:19:57 +0300

    added checkbox

--- app/src/main/java/suitecrm/com/todo/fragments/DoListFragment.kt
+++ app/src/main/java/suitecrm/com/todo/fragments/DoListFragment.kt
@@ -1,9 +1,14 @@
 package suitecrm.com.todo.fragments
 
+import android.graphics.Typeface
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import suitecrm.com.todo.R
+import suitecrm.com.todo.ui.app.elements.DoListViewBuilder
 
 
 /**
@@ -13,13 +18,28 @@ import suitecrm.com.todo.R
  */
 class DoListFragment : BaseFragment(R.layout.fragment_do_list) {
 
+//    override fun onResume() {
+//        super.onResume()
+//        val root = requireView().findViewById<LinearLayout>(R.id.mainToDoListDiv)
+//        for (i in 1..10) {
+//            var view = LayoutInflater.from(this.context).inflate(R.layout.to_do_row, root,false)
+//            view.id = (120 + i)
+//            root.addView(view)
+//        }
+//    }
+
     override fun onResume() {
         super.onResume()
         val root = requireView().findViewById<LinearLayout>(R.id.mainToDoListDiv)
-        for (i in 1..10) {
-            var view = LayoutInflater.from(this.context).inflate(R.layout.to_do_row, root,false)
-            view.id = (120 + i)
-            root.addView(view)
+        for (i in 1..100) {
+            if (activity?.applicationContext != null) {
+                var view1: LinearLayout = DoListViewBuilder()
+                        .createRowChecklistView(root.context)
+                //var view = LayoutInflater.from(this.context).inflate(R.layout.to_do_row, root,false)
+                view1.id = (120 + i)
+                root.addView(view1)
+            }
         }
     }
+
 }
---
+++ app/src/main/java/suitecrm/com/todo/ui/app/elements/DoListViewBuilder.kt
@@ -0,0 +1,35 @@
+package suitecrm.com.todo.ui.app.elements
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Typeface
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TextView
+import suitecrm.com.todo.R
+
+
+class DoListViewBuilder {
+    @SuppressLint("WrongConstant")
+    fun createRowChecklistView(context: Context): LinearLayout {
+        val row: LinearLayout = LinearLayout(context,null,0,R.style.TodoLinerRow)
+        row.orientation = LinearLayout.HORIZONTAL
+        row.layoutParams =  LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+        val checkBox: CheckBox = CheckBox(context,null,0,R.style.MainCheckBox)
+        checkBox.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        checkBox.isChecked = false
+
+
+        val textView: TextView = TextView(context,null,0, R.style.TodoTextViewMain)
+        textView.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+        textView.text= "какой то текс созданный программно"
+        row.addView(checkBox)
+        row.addView(textView)
+        return row
+    }
+}
