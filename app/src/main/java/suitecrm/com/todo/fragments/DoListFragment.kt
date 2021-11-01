80af9c45295b30f806a8fe0d8af39200b3a75638
SidorkinAlex <palach.orl@mail.ru>
2021-11-02 00:28:27 +0300

    deleted item after click to checkbox

--- app/src/main/java/suitecrm/com/todo/fragments/DoListFragment.kt
+++ app/src/main/java/suitecrm/com/todo/fragments/DoListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import suitecrm.com.todo.R
@@ -32,13 +33,14 @@ class DoListFragment : BaseFragment(R.layout.fragment_do_list) {
         super.onResume()
         val root = requireView().findViewById<LinearLayout>(R.id.mainToDoListDiv)
         for (i in 1..100) {
-            if (activity?.applicationContext != null) {
-                var view1: LinearLayout = DoListViewBuilder()
-                        .createRowChecklistView(root.context)
-                //var view = LayoutInflater.from(this.context).inflate(R.layout.to_do_row, root,false)
-                view1.id = (120 + i)
-                root.addView(view1)
-            }
+
+            var view1: LinearLayout = DoListViewBuilder()
+                    .createRowChecklistView(root.context)
+            //var view = LayoutInflater.from(this.context).inflate(R.layout.to_do_row, root,false)
+            view1.id = (120 + i)
+            root.addView(view1)
+
+
         }
     }
 
--- app/src/main/java/suitecrm/com/todo/ui/app/elements/DoListViewBuilder.kt
+++ app/src/main/java/suitecrm/com/todo/ui/app/elements/DoListViewBuilder.kt
@@ -3,6 +3,7 @@ package suitecrm.com.todo.ui.app.elements
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface
+import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.LinearLayout
@@ -13,21 +14,28 @@ import suitecrm.com.todo.R
 class DoListViewBuilder {
     @SuppressLint("WrongConstant")
     fun createRowChecklistView(context: Context): LinearLayout {
-        val row: LinearLayout = LinearLayout(context,null,0,R.style.TodoLinerRow)
+        val row: LinearLayout = LinearLayout(context, null, 0, R.style.TodoLinerRow)
         row.orientation = LinearLayout.HORIZONTAL
-        row.layoutParams =  LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+        row.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT)
-        val checkBox: CheckBox = CheckBox(context,null,0,R.style.MainCheckBox)
+        val checkBox: CheckBox = CheckBox(context, null, 0, R.style.MainCheckBox)
         checkBox.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT)
 
+
         checkBox.isChecked = false
+        checkBox.setOnClickListener {
+            var coreList = (row.parent)
+            if (coreList is LinearLayout) {
+                coreList.removeViewInLayout(row)
+            }
+            coreList.requestLayout()
+        }
 
-
-        val textView: TextView = TextView(context,null,0, R.style.TodoTextViewMain)
+        val textView: TextView = TextView(context, null, 0, R.style.TodoTextViewMain)
         textView.layoutParams = ViewGroup.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT)
-        textView.text= "какой то текс созданный программно"
+        textView.text = "какой то текс созданный программно"
         row.addView(checkBox)
         row.addView(textView)
         return row
