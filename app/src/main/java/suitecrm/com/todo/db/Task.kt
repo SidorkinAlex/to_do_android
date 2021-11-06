ebf4099400cbbd1f3428039a532e842a182f0c75
SidorkinAlex <palach.orl@mail.ru>
2021-11-07 01:10:41 +0300

    git updated data calss Task and create DAO Task

--- app/src/main/java/suitecrm/com/todo/db/Task.kt
+++ app/src/main/java/suitecrm/com/todo/db/Task.kt
@@ -4,12 +4,16 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.sql.Date
 
-@Entity
+@Entity(tableName = "task")
 data class Task(
-    @PrimaryKey(autoGenerate = true)
-    var id: Int,
-    var externalId: String,
-    var name: String,
-    var isChecker: Int,
-    val dateCreate: Date,
+        @PrimaryKey(autoGenerate = true)
+        var id: Int,
+        var externalId: String,
+        var name: String,
+        var categoryId: String,
+        var categoryName: String,
+        var isChecked: Int,
+        var isDeleted: Int,
+        var isNeedSync: Int,
+        val dateCreate: Date,
 )
---
+++ app/src/main/java/suitecrm/com/todo/db/TaskDaoInterface.kt
@@ -0,0 +1,27 @@
+package suitecrm.com.todo.db
+
+import androidx.room.*
+
+@Dao
+interface TaskDaoInterface {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertTask(task: Task)
+
+    @Update
+    fun updateTask(task: Task)
+
+    @Delete
+    fun deleteTask(task: Task)
+
+    @Query("SELECT * FROM task WHERE name == :id")
+    fun getTaskById(id: String): List<Task>
+
+    @Query("SELECT * FROM task WHERE `isChecked` = 0 AND `isDeleted`= 0")
+    fun getNonClosedTask(): List<Task>
+
+    @Query("SELECT * FROM task WHERE `isChecked` = 1 AND `isDeleted`= 0")
+    fun getClosedTask(): List<Task>
+
+    @Query("SELECT * FROM task WHERE `isDeleted`= 0" )
+    fun getDeletedTask(): List<Task>
+}
