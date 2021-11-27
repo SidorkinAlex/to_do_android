97e488f1563485befbf74a89e265b3b83097c68e
SidorkinAlex <palach.orl@mail.ru>
2021-11-27 23:26:39 +0300

    add db init

--- app/src/main/java/suitecrm/com/todo/MainActivity.kt
+++ app/src/main/java/suitecrm/com/todo/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import suitecrm.com.todo.databinding.ActivityMainBinding
+import suitecrm.com.todo.db.AppDatabase
+import suitecrm.com.todo.db.TaskDaoInterface
 import suitecrm.com.todo.fragments.DoListFragment
 import suitecrm.com.todo.ui.app.elements.AppDrawer
 
@@ -11,10 +13,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mAppDrawer: AppDrawer
     private lateinit var mToolbar: Toolbar
+    private lateinit var db:AppDatabase
+    private lateinit var taskDaoInterface: TaskDaoInterface
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+        initDB()
+    }
+
+    private fun initDB() {
     }
 
     override fun onStart() {
